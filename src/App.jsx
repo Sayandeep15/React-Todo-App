78246c360d7f3bb2c5ea93c448f5f9ea14d8e407
Sayandeep15 <sayandeepdutta83685@gmail.com>
2025-06-23 02:46:57 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -60,6 +60,16 @@ function App() {
     saveTodos();
   };
 
+  // DELETE ALL FUNCTION
+  const handleDltAll = () => {
+    const confirmDelete = window.confirm(
+      "Are you sure you want to delete all tasks?"
+    );
+    if (!confirmDelete) return; // user clicked "Cancel"
+    setTodos([]);
+    saveTodos();
+  };
+
   // ADD FUNCTION
   const handleAdd = () => {
     setTodos([...todos, { id: uuidv4(), todo, isCompleted: false }]);
@@ -94,7 +104,13 @@ function App() {
             : "light w-full h-screen text-white flex items-center"
         }
       >
-        <div className={!darkMode?"container  bg-[#131216] px-10 py-5 shadow-[0px_0px_20px_0px_rgba(255,_255,_255,_0.15)]  w-[600px] h-[600px] mx-auto  rounded-xl flex items-center flex-col":"containerLight px-10 py-5 shadow-[3px_0px_30px_0px_rgba(255,_255,_255,_0.75)]  w-[600px] h-[600px] mx-auto  rounded-xl flex items-center flex-col"}>
+        <div
+          className={
+            !darkMode
+              ? "container  bg-[#131216] px-10 py-5 shadow-[0px_0px_20px_0px_rgba(255,_255,_255,_0.15)]  w-[600px] h-[600px] mx-auto  rounded-xl flex items-center flex-col"
+              : "containerLight px-10 py-5 shadow-[3px_0px_30px_0px_rgba(255,_255,_255,_0.75)]  w-[600px] h-[600px] mx-auto  rounded-xl flex items-center flex-col"
+          }
+        >
           <h1 className="text-3xl mx-auto mb-4 font-semibold">
             Manage your todos at one place
           </h1>
@@ -110,38 +126,68 @@ function App() {
             <button
               onClick={handleAdd}
               disabled={todo.length <= 2}
-              className={!darkMode?"bg-[#313035] border-[1.5px] border-[#313035] py-1 px-4 rounded-xl hover:scale-105 font-semibold hover:bg-[#2D2C30] transition-all ease":"btnlight py-1 px-4 rounded-xl font-semibold hover:scale-105  transition-all ease"}
+              className={
+                !darkMode
+                  ? "bg-[#313035] border-[1.5px] border-[#313035] py-1 px-4 rounded-xl hover:scale-105 font-semibold hover:bg-[#2D2C30] transition-all ease"
+                  : "btnlight py-1 px-4 rounded-xl font-semibold hover:scale-105  transition-all ease"
+              }
             >
               Add
             </button>
           </div>
 
-          {/* SHOW FINISHED CHECKBOX */}
-          <div className=" w-full flex items-center mb-4 border-b">
-            <input
-              className="my-4  "
-              id="show"
-              onChange={toggleFinished}
-              type="checkbox"
-              checked={showFinished}
-            />
-            <label className="mx-2 text-xs text-blue-600 font-medium" htmlFor="show">
-              Show Finished
-            </label>
+          {/* SHOW FINISHED CHECKBOX  + DELETE ALL*/}
+          <div className="w-full flex justify-between mb-4 items-center border-b pb-2">
+            <div className=" w-fit items-center flex  ">
+              <input
+                className="my-2 "
+                id="show"
+                onChange={toggleFinished}
+                type="checkbox"
+                checked={showFinished}
+              />
+              <label
+                className="mx-2 text-xs text-blue-600 font-medium"
+                htmlFor="show"
+              >
+                Show Finished
+              </label>
+            </div>
+            <button
+              onClick={handleDltAll}
+              className={
+                !darkMode
+                  ? " flex items-center gap-1 border-[1.5px]  text-sm font-semibold text-red-500 py-1.5 px-3 rounded-xl hover:scale-105 hover:bg-[#2D2C30] transition-all ease"
+                  : "border boder-red-600  flex items-center gap-1 text-sm font-semibold text-red-600 py-1.5 px-3 rounded-xl hover:scale-105  transition-all ease"
+              }
+            >
+              Delete All
+              <MdDeleteOutline />
+            </button>
           </div>
 
           {/* show todos */}
           <p className="mr-auto mb-2 font-medium">Your Todos</p>
           <div className="todolist  text-white w-full overflow-y-auto overflow-x-hidden">
             {todos.length === 0 && (
-              <div className={!darkMode?"text-zinc-400 text-sm":"text-zinc-600 text-sm"}>No tasks to display</div>
+              <div
+                className={
+                  !darkMode ? "text-zinc-400 text-sm" : "text-zinc-600 text-sm"
+                }
+              >
+                No tasks to display
+              </div>
             )}
             {todos.map((item) => {
               return (
                 (showFinished || !item.isCompleted) && (
                   <div
                     key={item.id}
-                    className={!darkMode?"todo flex items-center w-full justify-between mb-2 bg-[#17161A] p-1.5 rounded-md" : "todo bg-[#50a7ffe8] flex items-center w-full justify-between mb-2 p-1.5 rounded-md"}
+                    className={
+                      !darkMode
+                        ? "todo flex items-center w-full justify-between mb-2 bg-[#17161A] p-1.5 rounded-md"
+                        : "todo bg-[#50a7ffe8] flex items-center w-full justify-between mb-2 p-1.5 rounded-md"
+                    }
                   >
                     {/* CHECK BOX */}
                     <div className="flex w-full gap-2 items-baseline wrap-anywhere mr-2 ">
@@ -167,7 +213,11 @@ function App() {
                         onClick={(e) => {
                           handleEdit(e, item.id);
                         }}
-                        className={!darkMode?"bg-[#313035] border-[1.5px] border-[#313035] py-1.5 px-4 rounded-xl hover:scale-105 hover:bg-[#2D2C30] transition-all ease": "btnlight py-1.5 px-4 rounded-xl hover:scale-105  transition-all ease"}
+                        className={
+                          !darkMode
+                            ? "bg-[#313035] border-[1.5px] border-[#313035] py-1.5 px-4 rounded-xl hover:scale-105 hover:bg-[#2D2C30] transition-all ease"
+                            : "btnlight py-1.5 px-4 rounded-xl hover:scale-105  transition-all ease"
+                        }
                       >
                         <BiSolidEdit />
                       </button>
@@ -175,7 +225,11 @@ function App() {
                         onClick={(e) => {
                           handleDlt(e, item.id);
                         }}
-                        className={!darkMode?"bg-[#313035] border-[1.5px] border-[#313035] text-red-500 py-1.5 px-4 rounded-xl hover:scale-105 hover:bg-[#2D2C30] transition-all ease" : "btnlight text-red-500 py-1.5 px-4 rounded-xl hover:scale-105  transition-all ease"}
+                        className={
+                          !darkMode
+                            ? "bg-[#313035] border-[1.5px] border-[#313035] text-red-500 py-1.5 px-4 rounded-xl hover:scale-105 hover:bg-[#2D2C30] transition-all ease"
+                            : "btnlight text-red-500 py-1.5 px-4 rounded-xl hover:scale-105  transition-all ease"
+                        }
                       >
                         <MdDeleteOutline />
                       </button>
